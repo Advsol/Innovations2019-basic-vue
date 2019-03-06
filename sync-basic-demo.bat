5866b4d7c5a172bd5b9ed1ae8eb15aebf2fac427
pmb5 <pmb5@users.noreply.github.com>
2019-03-06 11:53:41 +0000

    switched auth to a SAS key

--- sync-basic-demo.bat
+++ sync-basic-demo.bat
@@ -3,4 +3,4 @@
 SET mypath=%~dp0
 SET mypath=%mypath%index.html
 
-"%ProgramFiles(x86)%\Microsoft SDKs\Azure\AzCopy\AzCopy.exe" /source:%mypath% /Dest:https://asiprostorage.blob.core.windows.net/innovations/basic-vue/index.html /SetContentType /Y /XO /DestKey:[hidden]
+"%ProgramFiles(x86)%\Microsoft SDKs\Azure\AzCopy\AzCopy.exe" /source:%mypath% /Dest:https://asiprostorage.blob.core.windows.net/innovations/basic-vue/index.html /SetContentType /Y /XO /DestSAS:"?st=2019-03-06T11%%3A30%%3A20Z&se=2019-03-21T19%%3A30%%3A00Z&sp=rwdl&sv=2018-03-28&sr=c&sig=[hidden]nPOpTLsfybMFskLi7sKUwMPyTRR57GPNFqWq5rk%%3D"
